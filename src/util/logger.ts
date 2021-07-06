fa17836d7e83598f4d539e604b5f342fa07b5b7c
João Brito <joao@bitcapital.com.br>
2021-07-06 10:12:04 -0300

    feat: added colors to the console using chalk

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,4 +1,4 @@
-
+import chalk, { gray, red } from 'chalk';
 export interface LoggerOptions {
   prefix?: string;
 }
@@ -7,30 +7,42 @@ export class Logger {
   constructor(private readonly options: LoggerOptions) {}
 
   public info(...args: any[]): void {
-    const formatted = this.formatInput(...args);
+    const formatted = this.formatInput(chalk.bold('INFO:'), ...this.formatInputTypes(args));
 
     console.info(...formatted);
   }
 
   public error(...args: any[]): void {
-    const formatted = this.formatInput(...args);
+    const formatted = this.formatInput(chalk.red(chalk.bold('ERROR:'), ...this.formatInputTypes(args)));
 
     console.error(...formatted);
   }
 
   public log(...args: any[]): void {
-    const formatted = this.formatInput(...args);
+    const formatted = this.formatInput(chalk.bold('LOG:'), chalk.gray(...this.formatInputTypes(args)));
 
     if (process.env.LOG_LEVEL === 'debug') {
-      console.log(...formatted);
+      console.log(...formatted); 
     }
   }
 
   private formatInput(...args: any[]): any[] {
     let newArgs = args;
     if (this.options.prefix) {
-      newArgs = [`[${this.options.prefix}]`,...args]
+      newArgs = [chalk.blue(`[${this.options.prefix}]`), ...args]
     }
+
+    return newArgs;
+  }
+
+  private formatInputTypes(args: any[]) {
+    let newArgs = args.map(arg => {
+      if (typeof arg === 'number') {
+        return `${chalk.yellow(arg.toString())}`;
+      }
+
+      return arg;
+    });
 
     return newArgs;
   }
