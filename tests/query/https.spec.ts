5c3dcf5dcd56a4cc1ac1c8a4ce8d0677f98dd976
João Brito <joao@bitcapital.com.br>
2021-07-07 16:55:08 -0300

    tests: increase jest timeout for dns query with doh

--- tests/query/https.spec.ts
+++ tests/query/https.spec.ts
@@ -5,11 +5,12 @@ import * as mock from './query.mock.json';
 import Packet from 'native-dns-packet';
 import { MAX_UDP_PACKET_SIZE } from "../../src/interceptor/udp";
 
-const CLOUDFLARE_DNS_SERVER = '1.1.1.1';
+const CLOUDFLARE_DNS_SERVER = '1.0.0.1';
 let client: DNSQuery;
 let mockInstance = mock;
 
 describe('DNSQuery with DNS-Over-HTTPS and CloudFlare DNS', () => {
+  jest.setTimeout(15000);
   beforeAll(async () => {
     client = new DNSQuery({
       forwardServer: CLOUDFLARE_DNS_SERVER,
